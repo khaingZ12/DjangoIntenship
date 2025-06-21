55fcb03b60e58b9b04af160cb1f31535bdfc53f7
khaingZ12 <kzarlin025@gmail.com>
2025-06-22 01:29:40 +0630

    hello

---
+++ myproject/myapp/condition.py
@@ -0,0 +1,31 @@
+# if, if else, if elif  elif ... else
+
+mark = 10
+if mark > 50:
+    print("Pass")
+else:
+    print("Fail")
+
+age = 25
+if age <= 12:
+    print("Child")
+elif age <= 19:
+    print("Teenager")
+else:
+    print("Adult")
+    
+mark = 65
+if mark >= 80:
+    print("Credit")
+elif mark >= 60:
+    print("Pass")
+else:
+    print("Fail")
+
+age = 20
+if age >= 18:
+    print("Eligible to vote ")
+
+
+age = 19
+if age > 18: print("Eligible to Vote ")
