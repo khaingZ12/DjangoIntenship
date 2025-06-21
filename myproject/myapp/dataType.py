f0bc5c23d9e5ee87209c7e315d3e85f5e90dd660
khaingZ12 <kzarlin025@gmail.com>
2025-06-22 00:33:45 +0630

    Update dataType.py

--- myproject/myapp/dataType.py
+++ myproject/myapp/dataType.py
@@ -2,8 +2,22 @@
 # set = []
 # tuple = () ပြင်လို့မရ
 # dict = {'key': 'values'}
-list = [12,13,1.2,"hello"]
-tuple = (12,13,1.2,"hello")
-dict = {'name': 'mg mg', 'age': 25}
+# list = [12,13,1.2,"hello"]
+# tuple = (12,13,1.2,"hello")
+# dict = {'name': 'mg mg', 'age': 25}
+# print(dict)
 
-print(dict)
+
+# a = [12,10,5,8,14]
+# b = [0,1,2,3,4]   #indent
+# print(a[0])
+# print(a[1:3])    #range  ဆိုရင် (indent + 1)
+
+
+# dic = {'name': 'mg mg', 'age': 25}
+# print(dic['age'])
+# print(dic.values())
+
+
+list = ['apple', 'orange', 'car']
+
