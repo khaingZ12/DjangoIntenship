1ff2444b31e51b1bda4eafc02b992f85adffc891
khaingZ12 <kzarlin025@gmail.com>
2025-06-22 00:17:49 +0630

    Update dataType.py

---
+++ myproject/myapp/dataType.py
@@ -0,0 +1,9 @@
+# list = [] ပြင်လို့ရ
+# set = []
+# tuple = () ပြင်လို့မရ
+# dict = {'key': 'values'}
+list = [12,13,1.2,"hello"]
+tuple = (12,13,1.2,"hello")
+dict = {'name': 'mg mg', 'age': 25}
+
+print(dict)
