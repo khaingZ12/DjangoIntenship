a5f241b00a3a5f236f1056a25e35d77cc99ee1e9
khaingZ12 <kzarlin025@gmail.com>
2025-06-22 01:09:02 +0630

    hello

---
+++ myproject/myapp/dict.py
@@ -0,0 +1,13 @@
+d = {
+    'name': 'mg mg',
+    'address': 'mandalay',
+}
+a = d.get('name')
+b = d.keys()
+c = d.values()
+
+d ['phone'] = '09123654'      #  dict မှာထပ်ထည့်ချင်ရင်သုံး
+
+print(a)
+print(b)
+print(d)
