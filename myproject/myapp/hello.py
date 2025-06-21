85ee64005ac608ed586caef7885dfa733eed6d14
khaingZ12 <kzarlin025@gmail.com>
2025-06-21 23:17:42 +0630

    Update hello.py

--- myproject/myapp/hello.py
+++ myproject/myapp/hello.py
@@ -1,1 +1,11 @@
-print('hello world')
+# print('hello world')
+# print('hello, world')
+
+# if 10 > 5:
+    # print("This is true!")
+    # print("I am tab indentation")
+# print("I have no indentation")
+
+input("Enter username")
+input("Enter password")
+input("Enter email")
