90badebc83261cb3d31358ea0e6f8840e1137ef6
khaingZ12 <kzarlin025@gmail.com>
2025-06-22 00:07:48 +0630

    hello

--- myproject/myapp/hello.py
+++ myproject/myapp/hello.py
@@ -6,6 +6,58 @@
     # print("I am tab indentation")
 # print("I have no indentation")
 
-input("Enter username")
-input("Enter password")
-input("Enter email")
+# input("Enter username")
+# input("Enter password")
+# input("Enter email")
+
+# name = "Mg Mg"
+# nick = "Mg"
+# age = 25
+# city = "Mandalay"
+# a = int(age)
+# str, int, float    / format ပြောင်းရင်သုံး
+# type()
+# print(name)
+# print(name,"\n", age, "\n",city)
+# print(name + nick)
+# print(type(age))
+# print(type(name))
+# print(int(age))
+# print(type(a))
+
+
+# x = y = 3.14
+# z = "hi"
+# print(x,y,z)
+
+# a = b = c = 100
+# print(a,b,c)
+# x, y, z = 1, 2.5, "python"
+# print(x,y,z)
+
+
+# ans = 20+25
+# name = 'mg' + 'mg'
+# a = 25-10
+# b = 10*10
+# c = 28/4
+# d = 25%5
+# print(ans)
+# print(name)
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+
+
+a = 40
+#  +=
+b = a
+b += 20
+# -=
+b -= 10
+# *=
+b *= 5
+# %=
+b %= 10
+print(b)
