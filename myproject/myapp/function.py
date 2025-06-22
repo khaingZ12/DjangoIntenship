8387598a13b6616f85052dce2cf349e21bed5a4f
khaingZ12 <kzarlin025@gmail.com>
2025-06-22 08:59:29 +0630

    Update function.py

--- myproject/myapp/function.py
+++ myproject/myapp/function.py
@@ -1,3 +1,13 @@
 # def function_name(parameters):
     # statement             #body statement
     # return       #function return
+
+
+a = 20
+b = 30
+
+def add_number():
+    ans = a + b
+    print(ans)
+
+add_number()
