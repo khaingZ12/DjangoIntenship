f1c3392ab72ee077d716875627f5e127e5bc470c
khaingZ12 <kzarlin025@gmail.com>
2025-06-22 08:52:51 +0630

    Update loop.py

--- myproject/myapp/loop.py
+++ myproject/myapp/loop.py
@@ -1,10 +1,29 @@
 # for , while
 
-for i in range(1,11):
-    print(i)
+# for i in range(1,11):
+    # print(i)
 
-for a in range(10,101):
-    print(a)
+# for a in range(10,101):
+    # print(a)
 
-for b in range(10,101,10):
-    print(b)
+# for b in range(10,101,10):
+    # print(b)
+
+
+# mylist = ['apple','orange', 'car', 'bus', 'yangon']
+# mydict = {'name': 'mg mg','address': 'mandalay',}
+# for i in mydict.values():
+    # print(i)
+# for i,x in mydict.items():          #key ရော values ရော ထုတ်ချင်ရင်သုံး
+    # print(i,x)
+
+
+cnt = 0
+while (cnt < 3):
+    cnt = cnt + 1
+    print("Hello World")
+
+
+def function_name(parameters):
+    # statement             #body statement
+    return       #function return
