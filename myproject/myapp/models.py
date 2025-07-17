409b4ff8c126f5f021dcf9d5ee1e2fd57f38ef0e
khaingZ12 <kzarlin025@gmail.com>
2025-07-17 23:06:30 +0630

    o

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    post_body = models.TextField()
+
+    def __str__(self):
+        return self.title
+    
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.TextField()
+    phone = models.CharField(max_length=20)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+    
+
+class Myfriend(models.Model):
+    name = models.CharField(max_length=255)
+    phone = models.CharField(max_length=255)
+    address = models.TextField()
+    email = models.EmailField()
+    photo = models.ImageField(upload_to='photo/')
+    created = models.DateField()
+
+    def __str__(self):
+        return self.name
+
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+def postdata(request):
+    post_data = Post.objects.all()
+    context = {'post_data':post_data}
+    return render(request, 'posts.html', context)
+
+
+def myfriends(request):
+    friends = Myfriend.objects.all()
+    mylist = [1,2,3,4]
+    context = {'friends':friends, 'mylist':mylist}
+    return render(request, 'myfriend.html', context)
